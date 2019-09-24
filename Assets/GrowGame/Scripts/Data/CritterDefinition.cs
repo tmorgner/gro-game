a57b3732ae6028e8c85c19278b9bae020d9114fb
Thomas Morgner <git@tmorgner.com>
2019-09-24 16:23:52 +0100

    WIP

---
+++ Assets/GrowGame/Scripts/Data/CritterDefinition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.GrowGame.Scripts.Data
+{
+    public class CritterDefinition: ScriptableObject
+    {
+        [SerializeField]
+        private float velocity;
+
+        [SerializeField]
+        private float damageApplied;
+
+        [SerializeField]
+        private PlantPart targetPart;
+    }
+}
---
+++ Assets/GrowGame/Scripts/FlowerBedBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.GrowGame.Scripts.Data
+{
+    public class FlowerBedBehaviour: MonoBehaviour
+    {
+        [SerializeField]
+        PlantBehaviour plant;
+    }
+
+    public class GlobalGameState: MonoBehaviour
+    {
+        [SerializeField]
+        private List<FlowerBedBehaviour> flowerBeds;
+        [SerializeField]
+        private List<CritterDefinition> availableCritters;
+        [SerializeField]
+        private List<PlantDefinition> availablePlants;
+
+    }
+}
