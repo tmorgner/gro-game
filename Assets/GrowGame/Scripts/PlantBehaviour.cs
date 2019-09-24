6680f11e636177299cde36e50c1f9a0babbb0301
Thomas Morgner <git@tmorgner.com>
2019-09-24 16:23:52 +0100

    WIP

---
+++ Assets/GrowGame/Scripts/PlantBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.GrowGame.Scripts.Data
+{
+    public class PlantBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private PlantDefinition definition;
+
+        private readonly Dictionary<PlantPart, List<PlantPartBehaviour>> parts;
+
+        public PlantBehaviour()
+        {
+            parts = new Dictionary<PlantPart, List<PlantPartBehaviour>>();
+        }
+
+        protected virtual void Awake()
+        {
+            var rawParts = GetComponentsInChildren<PlantPartBehaviour>();
+            foreach (var p in rawParts)
+            {
+                if (parts.TryGetValue(p.Part, out var list))
+                {
+                    list.Add(p);
+                }
+                else
+                {
+                    parts.Add(p.Part, new List<PlantPartBehaviour>() {p});
+                }
+            }
+        }
+    }
+}
---
+++ Assets/GrowGame/Scripts/PlantPartBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.GrowGame.Scripts.Data
+{
+    public class PlantPartBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private PlantPart part;
+
+        public PlantPart Part => part;
+    }
+}
